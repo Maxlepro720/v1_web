a8c822d487f33ffaa47453126e4e22237b1b0283
Maxlepro720 <maxencecera@gmail.com>
2026-01-25 08:59:14 +0100

    upgrade, fix bug,encore

--- site/sitemap.php
+++ site/sitemap.php
@@ -1,27 +1,37 @@
 <?php
+// Désactiver TOUT affichage d'erreur (obligatoire pour XML)
+ini_set('display_errors', 0);
+error_reporting(0);
+
 header("Content-Type: application/xml; charset=UTF-8");
 
-$baseUrl = "https://projet2fdp.xo.je/";
+$baseUrl = "https://projet2fdp.xo.je";
 
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
 
 $directory = __DIR__;
-$files = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator($directory)
+
+$iterator = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($directory, FilesystemIterator::SKIP_DOTS)
 );
 
-foreach ($files as $file) {
-    if ($file->isFile() && pathinfo($file, PATHINFO_EXTENSION) === "html") {
-        $path = str_replace($directory, "", $file->getPathname());
-        $url = $baseUrl . str_replace("\\", "/", $path);
+foreach ($iterator as $file) {
+    if ($file->isFile() && strtolower($file->getExtension()) === "html") {
 
-        echo "<url>";
-        echo "<loc>$url</loc>";
-        echo "<lastmod>" . date("Y-m-d", filemtime($file)) . "</lastmod>";
-        echo "<priority>0.8</priority>";
-        echo "</url>";
+        $path = str_replace($directory, "", $file->getPathname());
+        $path = str_replace("\\", "/", $path);
+
+        // Évite sitemap.php, erreurs, etc.
+        if (strpos($path, '.html') === false) continue;
+
+        echo "  <url>\n";
+        echo "    <loc>$baseUrl$path</loc>\n";
+        echo "    <lastmod>" . date("Y-m-d", $file->getMTime()) . "</lastmod>\n";
+        echo "    <priority>0.8</priority>\n";
+        echo "  </url>\n";
     }
 }
 
 echo "</urlset>";
+exit;

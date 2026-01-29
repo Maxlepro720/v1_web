79e1223984fdb1ad16ad485055ac5bc440ad7e0d
Maxlepro720 <maxencecera@gmail.com>
2026-01-29 12:44:01 +0100

    upgrade, fix bug,encore

--- site/admin/ban/script_get_ban.js
+++ site/admin/ban/script_get_ban.js
@@ -1,26 +1,25 @@
 const API_URL = "https://project-3-api-2bgb.onrender.com";
-    const PLAYER_ID = localStorage.getItem("username"); // ou remplace par ton système
+const PLAYER_ID = localStorage.getItem("username"); // ou remplace par ton système
 
-    if (!PLAYER_ID) {
-        console.error("Aucun player_id trouvé");
-    } else {
-        async function checkBan() {
-            try {
-                const response = await fetch(`${API_URL}/get_ban?id=${PLAYER_ID}`);
-                const data = await response.json();
-
-                if (data.status === "success" && data.is_banned === true) {
-                    alert("Vous avez été banni. L'onglet va se fermer.");
-                    window.close();
-                }
-            } catch (error) {
-                console.error("Erreur lors de la vérification du ban :", error);
+if (!PLAYER_ID) {
+    console.error("Aucun player_id trouvé");
+} else {
+    async function checkBan() {
+        try {
+            const response = await fetch(`${API_URL}/get_ban?id=${PLAYER_ID}`);
+            const data = await response.json();
+            if (data.status === "success" && data.is_banned === true) {
+                alert("Vous avez été banni. L'onglet va se fermer.");
+                window.close();
             }
+        } catch (error) {
+            console.error("Erreur lors de la vérification du ban :", error);
         }
+    }
 
         // Vérification immédiate au chargement
-        checkBan();
+    checkBan();
 
         // Vérification toutes les 30 secondes
-        setInterval(checkBan, 30000);
-    }
+    setInterval(checkBan, 30000);
+}

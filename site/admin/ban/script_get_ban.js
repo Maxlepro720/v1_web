1ddf5056ac8b8f67e069453e1a705f28d5abf9f5
Maxlepro720 <maxencecera@gmail.com>
2026-01-29 12:45:30 +0100

    upgrade, fix bug,encore

--- site/admin/ban/script_get_ban.js
+++ site/admin/ban/script_get_ban.js
@@ -1,5 +1,6 @@
 const API_URL = "https://project-3-api-2bgb.onrender.com";
 const PLAYER_ID = localStorage.getItem("username"); // ou remplace par ton système
+console.log("start_ban_verification")
 
 if (!PLAYER_ID) {
     console.error("Aucun player_id trouvé");
@@ -8,6 +9,7 @@ if (!PLAYER_ID) {
         try {
             const response = await fetch(`${API_URL}/get_ban?id=${PLAYER_ID}`);
             const data = await response.json();
+            print(data)
             if (data.status === "success" && data.is_banned === true) {
                 alert("Vous avez été banni. L'onglet va se fermer.");
                 window.close();

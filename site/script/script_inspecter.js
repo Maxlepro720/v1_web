18948f906e219f338ad301f962cad939e0e5c374
Maxlepro720 <maxencecera@gmail.com>
2026-01-30 19:42:19 +0100

    upgrade, fix bug,encore

---
+++ site/script/script_inspecter.js
@@ -0,0 +1,19 @@
+
+const _u = localStorage.getItem('username');
+
+if (_u === atob('bWF4YnJ5eDEx')) {
+  // saut / bypass
+} else {
+  document.addEventListener("contextmenu", e => e.preventDefault());
+
+  document.addEventListener("keydown", e => {
+    if (
+      e.key === "F12" ||
+      (e.ctrlKey && e.shiftKey && ["I","J","C"].includes(e.key)) ||
+      (e.ctrlKey && e.key === "U")
+    ) {
+      e.preventDefault();
+      return false;
+    }
+  });
+}
